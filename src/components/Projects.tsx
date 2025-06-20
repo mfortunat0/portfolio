8a1d247dc9ba5d8f158a286ab985416d209437ac
mfortunat0 <matheus.xmaz10@gmail.com>
2025-06-20 13:38:39 -0300

    Adicionando PC clean

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,6 +2,7 @@ import VideosNah from "../assets/videosdonah.png";
 import TotemMidiaIndoor from "../assets/totem-midia-indoor.png";
 import PastelDoCabelo from "../assets/patel-do-cabelo.png";
 import BeatrizSennosAdvogada from "../assets/beatriz-sennos-advogada.png";
+import PCClean from "../assets/pc-clean.png";
 
 export function Projects() {
   return (
@@ -144,11 +145,49 @@ export function Projects() {
               </div>
             </div>
             <div className="p-6">
-              <h3 className="text-xl font-bold mb-2">Pastel do Cabelo</h3>
+              <h3 className="text-xl font-bold mb-2">
+                Beatriz Sennos Advogada
+              </h3>
               <p className="text-gray-600 mb-4">
                 Website sob medida para uma advocacia em Caraguatatuba, focado
                 em destacar seus serviços, transmitir confiança e atrair novos
                 clientes que buscam excelência jurídica na região.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                <span className="text-xs bg-gray-100 px-2 py-1 rounded">
+                  Node.js
+                </span>
+                <span className="text-xs bg-gray-100 px-2 py-1 rounded">
+                  React.js
+                </span>
+                <span className="text-xs bg-gray-100 px-2 py-1 rounded">
+                  HostGator
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className="project-card bg-white rounded-xl overflow-hidden shadow-md border border-gray-100 transition">
+            <div className="relative h-48 overflow-hidden">
+              <img
+                src={PCClean}
+                alt="Plataforma de E-commerce"
+                className="w-full h-full object-cover"
+              />
+              <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
+              <div className="absolute bottom-0 left-0 p-4">
+                <span className="bg-blue-500 text-white text-xs px-2 py-1 rounded-full">
+                  Frontend
+                </span>
+              </div>
+            </div>
+            <div className="p-6">
+              <h3 className="text-xl font-bold mb-2">PC Clean</h3>
+              <p className="text-gray-600 mb-4">
+                Site sob medida para uma empresa de informática em Ubatuba, com
+                o objetivo de destacar seus principais serviços, reforçar a
+                credibilidade da marca e conquistar clientes que buscam soluções
+                tecnológicas confiáveis e de alta qualidade.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
                 <span className="text-xs bg-gray-100 px-2 py-1 rounded">
